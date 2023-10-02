62392522edbcf3303f311ee9bd57b9a66d0fb2ec
Marven-HackersBl4khat <146468163+Marven-HackersBl4khat@users.noreply.github.com>
2023-10-02 13:29:53 +0100

    Add files via upload

---
+++ multiplication.py
@@ -0,0 +1,9 @@
+print("multiplication Table: ")
+print(" \nenter 'q' to exit: ")
+
+user_n = input ("\nyour numerator: ")
+
+user_d = input("your denominator: ")
+
+output = float(user_n) * float(user_d)
+print(f" {user_n} × {user_d} = {output} ")
